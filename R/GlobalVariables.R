eaaf5c3dcc1a6b4321ad67f291e32a56e80bd804
David Rach <drach@som.umaryland.edu>
2026-03-21 22:06:52 -0400

    Updates to Description file. All the dependencies are non-Bioconductor ones, so this would be a CRAN package?

---
+++ R/GlobalVariables.R
@@ -0,0 +1,4 @@
+utils::globalVariables(c("Code", "Date", "Day", "Instrument",
+ "Start", "Start_Hour", "Start_dt", "Stop", "Stop_Hour",
+ "Stop_dt", "TheInstrument", "User", 'day', 'everything',
+  "instrument"))
